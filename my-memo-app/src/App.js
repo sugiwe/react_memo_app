d6bc3fbf18a2439cc3dddb1ddda2b6d91399ba1a
hiromisugie <lozz.gggg@gmail.com>
2024-07-18 06:09:02 +0900

    最低限の表示とスタイリング

--- my-memo-app/src/App.js
+++ my-memo-app/src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list">
+        <ul>
+          <li>メモ1</li>
+          <li>メモ2</li>
+          <li>メモ3</li>
+          <li>＋</li>
+        </ul>
+      </div>
+      <div className="memo">
+        <textarea />
+        <button>更新</button>
+        <button>削除</button>
+      </div>
     </div>
   );
 }
