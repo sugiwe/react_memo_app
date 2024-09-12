6653527732e0219999743a3a775ea8d710b77106
hiromisugie <lozz.gggg@gmail.com>
2024-09-13 05:45:51 +0900

    静的な状態でログインボタンを設置

--- my-memo-app/src/MemoApp.js
+++ my-memo-app/src/MemoApp.js
@@ -55,6 +55,14 @@ export default function MemoApp() {
 
   return (
     <div className="MemoApp">
+    <header>
+      <div className="header-left">
+        <p>MeMoo!!</p>
+      </div>
+      <div className="header-right">
+        <button>ログイン</button>
+      </div>
+    </header>
       <List
         memos={memos}
         onAddClick={handleAddClick}
